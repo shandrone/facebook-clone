3bb5ecbcf685b32ad319e8a3ee28b9ee38f86b28
shandrone <shandrone@gmail.com>
2020-10-13 18:00:34 -0600

    Updates

---
+++ precache-manifest.c942fae0e898962f6557e9c34ca88145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e984d33d602b7899c29bea480131e9a",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "ea4791700382f46b5d39",
+    "url": "/facebook-clone/static/css/main.08025034.chunk.css"
+  },
+  {
+    "revision": "3bf1d7e1f70f0bc8094a",
+    "url": "/facebook-clone/static/js/2.89ce05ca.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/facebook-clone/static/js/2.89ce05ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4791700382f46b5d39",
+    "url": "/facebook-clone/static/js/main.2f62d165.chunk.js"
+  },
+  {
+    "revision": "7919abc8259577748922",
+    "url": "/facebook-clone/static/js/runtime-main.7e1f1582.js"
+  }
+]);
